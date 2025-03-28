e899f80c871c64709e46124f776d99418a22308d
Lucas Declercq <lucas.declercq@agicap.com>
2025-03-28 17:04:17 +0100

    feat(frontend): add oauth

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 
-const API_BASE_URL = '/';
+export default ({ mode }) => {
+	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
-export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
-		},
-	},
-	server: {
-		proxy: {
-			'/api/v1': {
-				target: API_BASE_URL,
-				secure: true,
+	return defineConfig({
+		plugins: [vue()],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
 			},
 		},
-	},
-});
+		server: {
+			proxy: {
+				'/api/v1': {
+					target: process.env.VITE_API_BASE_URL || '/',
+					secure: true,
+					changeOrigin: true,
+				},
+			},
+		},
+	});
+}
