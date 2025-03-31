06c3f4d6db07331ddb776c437c1a13ee1d2689ff
Paul Delafosse <paul.delafosse@protonmail.com>
2025-03-31 07:22:22 +0200

    chore: fix lints

--- crates/elder-scrobz-api/src/test_helper.rs
+++ crates/elder-scrobz-api/src/test_helper.rs
@@ -1,6 +1,4 @@
-use crate::oauth::client::Oauth2Client;
 use crate::settings::Settings;
-use crate::state::AppState;
 use elder_scrobz_db::{build_pg_pool, migrate_db};
 use std::fs;
 use std::path::PathBuf;
@@ -23,15 +21,10 @@ pub async fn start_postgres() -> anyhow::Result<ContainerAsync<Postgres>> {
         .with_password("scrobz")
         .start()
         .await?;
-
     let host_port = container.get_host_port_ipv4(5432).await?;
     let host_ip = container.get_host().await?;
     let admin_db_url = format!("postgres://scrobz:scrobz@{host_ip}:{host_port}/scrobz");
-
     let pool = build_pg_pool(&admin_db_url).await;
-
-    let settings = Arc::new(Settings::get()?);
-
     migrate_db(&pool).await?;
     Ok(container)
 }
