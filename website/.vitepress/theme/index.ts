bd0ac42691543a05856c5099b5a8f1bb5389849f
Paul Delafosse <paul.delafosse@protonmail.com>
2025-03-31 06:18:57 +0200

    ci: add release action for later

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -1,35 +1,32 @@
 // https://vitepress.dev/guide/custom-theme
-import {h} from 'vue'
-import type {Theme} from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
-import {theme, useOpenapi, useTheme} from 'vitepress-openapi/client'
-import spec from '../../src/openapi.json'
-import 'vitepress-openapi/dist/style.css'
+import { h } from "vue";
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import "./style.css";
+import { theme, useOpenapi, useTheme } from "vitepress-openapi/client";
+import spec from "../../api-docs/openapi.json";
+import "vitepress-openapi/dist/style.css";
 
 export default {
-    extends: DefaultTheme,
-    Layout: () => {
-        return h(DefaultTheme.Layout, null, {
-            // https://vitepress.dev/guide/extending-default-theme#layout-slots
-        })
-    },
-    enhanceApp({app, router, siteData}) {
-        const openapi = useOpenapi({
-            spec,
-            config: {}
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    });
+  },
+  enhanceApp({ app, router, siteData }) {
+    const openapi = useOpenapi({
+      spec,
+      config: {},
+    });
 
-        });
+    useTheme({
+      operation: {
+        hiddenSlots: ["playground"],
+        cols: 1,
+      },
+    });
 
-        useTheme({
-            operation: {
-                hiddenSlots: [
-                    'playground',
-                ],
-                cols: 1,
-            }
-        });
-
-        theme.enhanceApp({app, openapi})
-    }
-} satisfies Theme
+    theme.enhanceApp({ app, openapi });
+  },
+} satisfies Theme;
