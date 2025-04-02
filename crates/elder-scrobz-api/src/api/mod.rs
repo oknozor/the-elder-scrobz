967add990beced4799385539683d1a6af2650475
Lucas Declercq <lucas.declercq@agicap.com>
2025-04-02 20:45:35 +0200

    docs: add bearer auth on swagger doc

--- crates/elder-scrobz-api/src/api/mod.rs
+++ crates/elder-scrobz-api/src/api/mod.rs
@@ -2,7 +2,7 @@ use crate::api::imports::*;
 use crate::oauth::AuthenticatedUser;
 use axum::middleware::from_extractor;
 use serde::Serialize;
-use utoipa::openapi::security::{ApiKey, ApiKeyValue, SecurityScheme};
+use utoipa::openapi::security::{ApiKey, ApiKeyValue, SecurityScheme, HttpBuilder, HttpAuthScheme};
 use utoipa::{Modify, OpenApi, ToSchema};
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_axum::routes;
@@ -43,8 +43,13 @@ impl Modify for SecurityAddon {
     fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
         if let Some(components) = openapi.components.as_mut() {
             components.add_security_scheme(
-                "api_key",
-                SecurityScheme::ApiKey(ApiKey::Header(ApiKeyValue::new("todo_apikey"))),
+                "bearer_auth",
+                SecurityScheme::Http(
+                    HttpBuilder::new()
+                        .scheme(HttpAuthScheme::Bearer)
+                        .bearer_format("JWT")
+                        .build()
+                ),
             )
         }
     }
