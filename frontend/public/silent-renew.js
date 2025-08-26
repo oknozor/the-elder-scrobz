1f356f93bbb46b2c11242fac9b5263f7630395b4
Paul Delafosse <paul.delafosse@protonmail.com>
2025-08-26 11:53:28 +0200

    chore(auth): cleanup silent renew

--- frontend/public/silent-renew.js
+++ frontend/public/silent-renew.js
@@ -1,15 +1,5 @@
 (() => {
   if (window.parent?.oidcUserManager) {
-    console.log("Processing silent renew callback");
-    window.parent.oidcUserManager
-      .signinSilentCallback()
-      .then(() => {
-        console.log("Silent renew completed successfully");
-      })
-      .catch((error) => {
-        console.error("Silent renew error:", error);
-      });
-  } else {
-    console.error("Unable to access UserManager from parent window");
+    window.parent.oidcUserManager.signinSilentCallback();
   }
 })();
