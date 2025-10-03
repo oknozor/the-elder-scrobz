3e684ce6db130acb2235e2820ba45bd3c616fec8
Paul Delafosse <paul.delafosse@protonmail.com>
2025-10-03 13:11:22 +0200

    feat(frontend): only one playing now per user

--- frontend/src/stores/eventStore.ts
+++ frontend/src/stores/eventStore.ts
@@ -6,6 +6,16 @@ export const useSseStore = defineStore("sse", () => {
     const messages = ref<ScrobzEvent[]>([]);
 
     const addMessage = (msg: ScrobzEvent) => {
+        if (msg.type === "PlayingNow") {
+            messages.value = messages.value.filter(
+                (existingMsg) =>
+                    !(
+                        existingMsg.type === "PlayingNow" &&
+                        existingMsg.user === msg.user
+                    ),
+            );
+        }
+
         messages.value.push(msg);
 
         if (msg.track_duration) {
