5e1f053b85021ead8f4f61d1c14d02439a96df2f
Paul Delafosse <paul.delafosse@protonmail.com>
2025-11-02 10:31:32 +0100

    fix(frontend): remove frontend oidc config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-echo "Generating frontend environment configuration..."
-
-cat > /app/frontend/config.js << EOF
-window.env = {
-  VITE_OIDC_AUTHORITY: "${SCROBZ_OPENID_AUTHORITY}",
-  VITE_OIDC_CLIENT_ID: "${SCROBZ_OPENID_CLIENT_ID}",
-  VITE_OIDC_CLIENT_SECRET: "${SCROBZ_OPENID_CLIENT_SECRET}"
-};
-EOF
-
-echo "Frontend environment configuration generated at /app/frontend/config.js"
-
 echo "Starting application..."
 
 exec "$@"
